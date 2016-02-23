b70f7a6761352ac77af23e72dc21160f34aa74a8
ronnchyran <ronny6993@gmail.com>
2016-02-23 09:19:01 -0500

    Initial Commit

---
+++ com/ronnchyran/ics3u1/ipo/RonnyChanBG.java
@@ -0,0 +1,25 @@
+/* 
+	File Name:   RonnyChanBG.java
+	Name:        Ronny Chan
+	Class:       ICS3U1-01 (B)
+	Date:        February 22, 2016
+	Description: This prints out a short blurb on the student's backgroun
+	
+*/
+public class RonnyChanBG
+{
+
+	public static void main(String[] args)
+	{
+		String studentName = "Ronny Chan";
+		String teacherName = "Mr. Lees";
+		String javaKnowledge = "Java source code compiles to bytecode which is run by the Java Virtual Machine";
+		int finalMarkHope = 90;
+		System.out.println("\t\t  " + studentName + "\n"); //given \t is the width of 6 spaces, \t\t^^ = 14 spaces
+		System.out.println(teacherName + " was my ICS2O1 teacher.");
+		System.out.println(javaKnowledge);
+		System.out.println("I hope to achieve " + finalMarkHope + "% in ICS3U1");
+	
+	}//static void main
+	
+}//class RonnyChanBG
---
+++ res/ProgramHeaderExample.java
@@ -0,0 +1,24 @@
+//	File Name:   SayHello.java
+//	Name:        Ms. Heffernan
+//	Class:       ICS3U1-01 (D)
+//	Date:        September 18, 2015
+//	Description: This program displays the word "Hello".
+
+	
+
+
+
+
+/* 
+	File Name:   MarksAndAverage.java
+	Name:        Ms. Heffernan
+	Class:       ICS3U1-01 (A)
+	Date:        February 14, 2014
+	Description: This program prompts user for a student's four marks, then
+	             calculates and outputs the average.
+	
+*/
+ 
+ 
+ 
+ 
